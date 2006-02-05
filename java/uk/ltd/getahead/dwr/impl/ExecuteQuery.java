e3219111c6c6a3d3e766883913dbfa4be145dfaf
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2006-02-05 14:23:37 +0000

    Attempt at Safari fix - breaks PC, but maybe better on a Mac
    git-svn-id: http://svn.directwebremoting.org/branches/Branch_1_1@506 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java/uk/ltd/getahead/dwr/impl/ExecuteQuery.java
+++ java/uk/ltd/getahead/dwr/impl/ExecuteQuery.java
@@ -293,14 +293,10 @@ public class ExecuteQuery
                 break;
             }
 
-            // If there are any &s then this must be iframe post and all the
-            // parameters have got dumped on one line.
-            if (line.indexOf('&') == -1)
+            if (line.indexOf('&') != -1)
             {
-                parsePostLine(line, paramMap);
-            }
-            else
-            {
+                // If there are any &s then this must be iframe post and all the
+                // parameters have got dumped on one line, split with &
                 log.debug("Using iframe POST mode"); //$NON-NLS-1$
                 StringTokenizer st = new StringTokenizer(line, "&"); //$NON-NLS-1$
                 while (st.hasMoreTokens())
@@ -311,22 +307,28 @@ public class ExecuteQuery
                     parsePostLine(part, paramMap);
                 }
             }
-        }
-
-        // Safari gets confused so we need some special treatment
-        // See: http://bugzilla.opendarwin.org/show_bug.cgi?id=3565
-        //      https://dwr.dev.java.net/issues/show_bug.cgi?id=93
-        //      http://jira.atlassian.com/browse/JRA-8354
-        //      http://developer.apple.com/internet/safari/uamatrix.html
-        String params = req.getParameter(ConversionConstants.BROKEN_SAFARI2);
-        if (params != null && params.length() > 0)
-        {
-            StringTokenizer st = new StringTokenizer(params, "\n"); //$NON-NLS-1$
-            while (st.hasMoreTokens())
+            else if (line.indexOf('\n') == -1)
             {
-                String part = st.nextToken();
-                part = LocalUtil.decode(part);
-                parsePostLine(part, paramMap);
+                // If there are any \ns then this must be a broken Safari. All
+                // the parameters have got dumped on one line split with \n
+                // See: http://bugzilla.opendarwin.org/show_bug.cgi?id=3565
+                //      https://dwr.dev.java.net/issues/show_bug.cgi?id=93
+                //      http://jira.atlassian.com/browse/JRA-8354
+                //      http://developer.apple.com/internet/safari/uamatrix.html
+                log.debug("Using Broken Safari POST mode"); //$NON-NLS-1$
+                StringTokenizer st = new StringTokenizer(line, "\n"); //$NON-NLS-1$
+                while (st.hasMoreTokens())
+                {
+                    String part = st.nextToken();
+                    part = LocalUtil.decode(part);
+
+                    parsePostLine(part, paramMap);
+                }
+            }
+            else
+            {
+                // Horay, this is a normal one!
+                parsePostLine(line, paramMap);
             }
         }
 
