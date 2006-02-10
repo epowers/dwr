158aa2b2c99786b9791cd66efad166d32b4574b6
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2006-02-10 19:08:38 +0000

    All links in demo app should be absolute
    git-svn-id: http://svn.directwebremoting.org/branches/Branch_1_1@513 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- java/uk/ltd/getahead/dwr/impl/DefaultIndexProcessor.java
+++ java/uk/ltd/getahead/dwr/impl/DefaultIndexProcessor.java
@@ -59,7 +59,7 @@ public class DefaultIndexProcessor implements Processor
         {
             String name = (String) it.next();
             Creator creator = creatorManager.getCreator(name);
-            out.println("<li><a href='." + HtmlConstants.PATH_TEST + name + "'>" + name + "</a> (" + creator.getType().getName() + ")</li>"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+            out.println("<li><a href='" + req.getContextPath() + req.getServletPath() + HtmlConstants.PATH_TEST + name + "'>" + name + "</a> (" + creator.getType().getName() + ")</li>"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
         }
         out.println("</ul>"); //$NON-NLS-1$
 
