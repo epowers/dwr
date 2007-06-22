7c6279eca264344110f39e1d017c22004745e2d0
joe_walker <joe_walker@3a8262b2-faa5-11dc-8610-ff947880b6b2>
2007-06-22 20:41:17 +0000

    removing jetty launchers that were of no use, fixing the one that was
    git-svn-id: http://svn.directwebremoting.org/branches/Branch_2_0@1386 3a8262b2-faa5-11dc-8610-ff947880b6b2

--- demo/org/getahead/dwrdemo/cli/JettyLauncher.java
+++ demo/org/getahead/dwrdemo/cli/JettyLauncher.java
@@ -1,50 +1,31 @@
 package org.getahead.dwrdemo.cli;
 
-import org.directwebremoting.servlet.DwrServlet;
 import org.mortbay.jetty.Server;
-import org.mortbay.jetty.handler.ResourceHandler;
 import org.mortbay.jetty.nio.SelectChannelConnector;
-import org.mortbay.jetty.servlet.Context;
-import org.mortbay.jetty.servlet.ServletHolder;
+import org.mortbay.jetty.webapp.WebAppContext;
 
 /**
- * JettyLauncher.
+ * Launch Jetty embedded.
  */
 public class JettyLauncher
 {
     /**
      * Sets up and runs server.
-     * @param args
+     * @param args The command line arguments
+     * @throws Exception Don't care because top level
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         Server server = new Server();
 
         SelectChannelConnector connector = new SelectChannelConnector();
         connector.setPort(8080);
         server.addConnector(connector);
+        server.setStopAtShutdown(true);
 
-        Context htmlContext = new Context(server, "/", Context.SESSIONS);
-        ResourceHandler htmlHandler = new ResourceHandler();
-        htmlHandler.setResourceBase("web");
-        htmlContext.setHandler(htmlHandler);
+        server.addHandler(new WebAppContext("web","/dwr"));
 
-        Context servletContext = new Context(server, "/", Context.SESSIONS);
-        ServletHolder holder = new ServletHolder(new DwrServlet());
-        holder.setInitParameter("activeReverseAjaxEnabled", "true");
-        holder.setInitParameter("debug", "true");
-        servletContext.addServlet(holder, "/dwr/*");
-        servletContext.setResourceBase("web");
-
-        try
-        {
-            JettyShutdown.addShutdownHook(server);
-            server.start();
-            server.join();
-        }
-        catch (Exception ex)
-        {
-            ex.printStackTrace();
-        }
+        server.start();
+        server.join();
     }
 }
